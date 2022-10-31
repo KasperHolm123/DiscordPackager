9017c5a8e8992a40d452b29257543a36da26251b
Kasper Holm <kasper.marhauer@gmail.com>
2022-10-31 13:51:16 +0100

    introduced .env, updated .gitignore

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from struct import pack
 from fetch import packager
+import os
+from dotenv import load_dotenv
+
+# Load environmental variables
+load_dotenv(dotenv_path=".env")
+
+auth_details = os.getenv('AUTH')
 
 # See README.md if you don't know how to get these data
+
+# dotenv approach
+credentials_dotenv = {
+    "Authorization": f"{auth_details}",
+    "Server" : "YOUR SERVER DATA",
+    "Channels": {}
+}
+
+# dangerous approach
 credentials = {
-    "Authorization": "YOUR AUTHROIZATION DATA",
+    "Authorization": "YOUR AUTHORIZATION DATA",
     "Server" : "YOUR SERVER DATA",
     "Channels": {}
 }
 
 if __name__ == "__main__":
-    client = packager(credentials)
+    client = packager(credentials_dotenv)
     client.fetch_content()
